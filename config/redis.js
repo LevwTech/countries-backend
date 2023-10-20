41b6b0dd9b68823dd5d0815d257bb34d7c820c1d
levwtech <abdelraahmanmostafa@gmail.com>
2023-10-20 13:41:18 +0300

    fix: redis connection in prod

--- config/redis.js
+++ config/redis.js
@@ -2,8 +2,10 @@ const Redis = require('ioredis');
 const { createClient } = require('redis');
 let redis;
 if (process.env.ENV === 'dev') {
+  console.log('dev');
   redis = new Redis(process.env.REDIS_URL);
 } else {
+  console.log('prod');
   redis = createClient({
     password: process.env.REDIS_PW,
     socket: {
@@ -11,6 +13,9 @@ if (process.env.ENV === 'dev') {
       port: process.env.REDIS_PORT,
     },
   });
+  (async () => {
+    await redis.connect();
+  })();
 }
 
 module.exports = redis;
