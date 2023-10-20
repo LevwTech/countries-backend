7ac5a954186a5fcb85336df2ed70dd68f74ff3cb
levwtech <abdelraahmanmostafa@gmail.com>
2023-10-20 13:41:49 +0300

    fix: remove redis logs

--- config/redis.js
+++ config/redis.js
@@ -2,10 +2,8 @@ const Redis = require('ioredis');
 const { createClient } = require('redis');
 let redis;
 if (process.env.ENV === 'dev') {
-  console.log('dev');
   redis = new Redis(process.env.REDIS_URL);
 } else {
-  console.log('prod');
   redis = createClient({
     password: process.env.REDIS_PW,
     socket: {
